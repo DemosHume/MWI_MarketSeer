846b8cf651257838d6bdb345d4d95f5c84d86c72
Demos <10341165+demoshume@user.noreply.gitee.com>
2025-04-24 15:51:30 +0800

    优化训练模型函数，增加回归和分类损失的计算与输出，并定期保存模型

--- BiLSTM_Attention_Hike/train.py
+++ BiLSTM_Attention_Hike/train.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
-def train_model(x_data, y_data, input_size, n_future, device, model=None,epoch=100,lr=0.001):
+def train_model(x_data, y_data, input_size, n_future, device, model=None, epoch=100, lr=0.001):
     from data import PriceDataset
     from model import DualHeadBiLSTM
 
@@ -22,14 +22,28 @@ def train_model(x_data, y_data, input_size, n_future, device, model=None,epoch=1
     for epoch in tqdm(range(epoch)):
         model.train()
         total_loss = 0
+        total_reg_loss = 0
+        total_cls_loss = 0
+
         for x, y, cls in loader:
             reg_out, cls_out = model(x)
-            loss = mse_loss(reg_out, y) + 0.5 * bce_loss(cls_out, cls)
+            reg_loss = mse_loss(reg_out, y)  # 回归损失
+            cls_loss = bce_loss(cls_out, cls)  # 分类损失
+            loss = 0.1*reg_loss + 0.9*cls_loss
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
             total_loss += loss.item()
-        print(f"Epoch {epoch+1} | Loss: {total_loss / len(loader):.4f}")
+            total_reg_loss += reg_loss.item()
+            total_cls_loss += cls_loss.item()
+        # 打印每个 epoch 的损失
+        print(f"Epoch {epoch+1} | Total Loss: {total_loss / len(loader):.4f} | "
+              f"Regression Loss: {total_reg_loss / len(loader):.4f} | "
+              f"Classification Loss: {total_cls_loss / len(loader):.4f}")
+
+        # 每隔 10 epoch 保存模型
         if (epoch+1) % 10 == 0:
             torch.save(model.state_dict(), f'model.pth')
             print(f"Model saved at epoch {epoch+1}")
